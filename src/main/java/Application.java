197c0d785aa291b02f9697ac1cd77ee58f6d1353
unknown <perryluigi16@gmail.com>
2020-04-15 15:53:00 -0500

    Done with my Query

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,21 +1,4 @@
 import data.DataBuilder;
-<<<<<<< HEAD
-import functions.FunctionForQ;
-import models.*;
-=======
-import functions.DocLinkFunctions;
-import models.Post;
-import models.PostType;
-import models.User;
-
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
->>>>>>> d987a1264e6ba0938c298e100e4089f82a03417b
-
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Application  {
     public static void main(String[] args) {
@@ -27,7 +10,7 @@ public class Application  {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        
+
     }
 
 
