09b553392e1408918ef3dca3b173919618be9bf2
Rina Tamrakar <rina@Rinas-MacBook-Pro.local>
2020-04-15 17:07:29 -0500

    Few changes for doctors

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,38 @@
 import data.DataBuilder;
+import functions.GroupByLocation;
+import models.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 public class Application {
+
+    private static List<Post> posts;
+
     public static void main(String[] args) {
+
+        posts = DataBuilder.getPosts();
         Generator generator = new Generator();
         generator.run();
 
+
         try {
             DataBuilder.setUp();
+
+
+            System.out.println();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
+        Map<Country, List<String>> listLocation = GroupByLocation.groupByLocation.apply(posts, PostType.HEALTH_ISSUE);
+        System.out.println(listLocation);
+
     }
+
 }
+
+
+
---
+++ src/main/java/functions/NonActiveDoctors.java
@@ -0,0 +1,29 @@
+package functions;
+
+import data.DataBuilder;
+import models.Comment;
+import models.Doctor;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class NonActiveDoctors {
+//    public static final Function<List<Doctor>, List<Doctor>> nonActiveDocs =
+//            (doctors -> doctors.stream()
+//                .collect(Collectors.toMap(doctor -> doctor, entry-> DataBuilder.getPosts()))
+//                .entrySet()
+//                .stream()
+//                    .filter(p -> p.getValue().getUser().getId().equals(doctors.getUser().getId()))
+//
+//
+//                return null;
+//            );
+
+
+        
+
+
+
+}
